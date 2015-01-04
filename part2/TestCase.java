83c54bdb7d6b2ae92cf33e540cedef719b2fba78
IYousuf <imad_yousuf@yahoo.com>
2015-01-04 15:48:55 +0500

    Update TestCase.java

--- part2/TestCase.java
+++ part2/TestCase.java
@@ -8,7 +8,11 @@ public class TestCase {
 		this.name = name;
 	}
 	
+	public void setUp(){
+	}
+	
 	public void run() throws Exception{
+		setUp();
 		this.getClass().getMethod("testMethod").invoke(this);
 	}
 }
